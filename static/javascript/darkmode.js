edefc11e788b6caba5799849e88182ac30ff6f93
darksilicon13 <darksilicon@outlook.com>
2022-05-16 00:23:24 +0900

    Applied approriate dark-mode and bright-mode to playlist page

--- static/javascript/darkmode.js
+++ static/javascript/darkmode.js
@@ -3,39 +3,60 @@ const toggleLable = document.querySelector(".label");
 const toggleBall = document.querySelector(".ball");
 const names = document.querySelectorAll(".videoText");
 
-$(document).ready(function(){
+$(document).ready(function () {
   var date = new Date();
   var time = date.getHours();
-  if(window.sessionStorage.getItem("darkmode") === null ) window.sessionStorage.setItem("darkmode", "realtime");
+  if (window.sessionStorage.getItem("darkmode") === null) window.sessionStorage.setItem("darkmode", "realtime");
 
-  if (window.sessionStorage.getItem("darkmode") === 'realtime') time >= 18 || time < 7  ? getDark() : getBright();
-  else if(window.sessionStorage.getItem("darkmode")==="true") getDark();
-  else if(window.sessionStorage.getItem("darkmode")=== "false") getBright();
+  if (window.sessionStorage.getItem("darkmode") === 'realtime') time >= 18 || time < 7 ? getDark() : getBright();
+  else if (window.sessionStorage.getItem("darkmode") === "true") {
+    getDark();
+    $("#comment-section").css('color', 'white');
+    $('#sidebar').css('background-color', 'black');
+    $('#prev-button').css('filter', 'none');
+    $('#next-button').css('filter', 'none');
+  }
+  else if (window.sessionStorage.getItem("darkmode") === "false") {
+    getBright();
+    $("#comment-section").css('color', 'black');
+    $('#sidebar').css('background-color', 'white');
+    $('#prev-button').css('filter', 'invert(100%)');
+    $('#next-button').css('filter', 'invert(100%)');
+  }
 });
 
 
 
-checkbox.addEventListener('change', ()=>{
-    if( toggleLable.classList.contains("toggleLable") ){
-        window.sessionStorage.setItem("darkmode", "false");
-        getBright()
-    }else{
-      window.sessionStorage.setItem("darkmode", "true");
-      getDark()
-    }
+checkbox.addEventListener('change', () => {
+  if (toggleLable.classList.contains("toggleLable")) {
+    window.sessionStorage.setItem("darkmode", "false");
+    getBright();
+    $("#comment-section").css('color', 'black');
+    $('#sidebar').css('background-color', 'white');
+    $('#prev-button').css('filter', 'invert(100%)');
+    $('#next-button').css('filter', 'invert(100%)');
+
+  } else {
+    window.sessionStorage.setItem("darkmode", "true");
+    getDark();
+    $("#comment-section").css('color', 'white');
+    $('#sidebar').css('background-color', 'black');
+    $('#prev-button').css('filter', 'none');
+    $('#next-button').css('filter', 'none');
+  }
 })
 
-function getDark(){
+function getDark() {
   var path = window.location.pathname;
   var page = path.split("/").pop();
   document.body.classList.add("dark");
   toggleLable.classList.add("toggleLable");
   toggleBall.classList.add("toggleBall");
   // if (document.getElementById('authorName')!== null) 
-  if(page === "playlist.html") {
-    document.getElementById('authorName').style.color='white'; 
+  if (page === "playlist.html") {
+    document.getElementById('authorName').style.color = 'white';
   }
-  else if(page === "main.html"){
+  else if (page === "main.html") {
     var list;
     list = document.querySelectorAll(".videoText > a");
     console.log(list);
@@ -44,37 +65,37 @@ function getDark(){
       list[i].classList.add('toggleWhite');
     }
   }
-  else{
-      if(page === "request.html"){
-        var list;
-        list = document.querySelectorAll(".youtuberLink");
-        for (var i = 0; i < list.length; ++i) {
+  else {
+    if (page === "request.html") {
+      var list;
+      list = document.querySelectorAll(".youtuberLink");
+      for (var i = 0; i < list.length; ++i) {
         list[i].classList.remove('toggleBlack');
         list[i].classList.add('toggleWhite');
       }
-        }
+    }
     document.body.classList.add("dark");
   }
 }
 
-function getBright(){
-    var path = window.location.pathname;
-    var page = path.split("/").pop();
+function getBright() {
+  var path = window.location.pathname;
+  var page = path.split("/").pop();
+  document.body.classList.remove("dark");
+  toggleLable.classList.remove("toggleLable");
+  toggleBall.classList.remove("toggleBall");
+  // if (document.getElementById('authorName')!== null) 
+  if (page === "playlist.html") document.getElementById('authorName').style.color = 'black';
+  else if (page === "main.html") {
+    var list;
+    list = document.querySelectorAll(".videoText > a");
+    console.log(list);
+    for (var i = 0; i < list.length; ++i) {
+      list[i].classList.remove('toggleWhite');
+      list[i].classList.add('toggleBlack');
+    }
+  }
+  else {
     document.body.classList.remove("dark");
-    toggleLable.classList.remove("toggleLable");
-    toggleBall.classList.remove("toggleBall");
-    // if (document.getElementById('authorName')!== null) 
-    if(page === "playlist.html") document.getElementById('authorName').style.color='black';
-    else if(page === "main.html"){
-      var list;
-      list = document.querySelectorAll(".videoText > a");
-      console.log(list);
-      for (var i = 0; i < list.length; ++i) {
-        list[i].classList.remove('toggleWhite');
-        list[i].classList.add('toggleBlack');
-      }
-    }
-    else{
-      document.body.classList.remove("dark");
-    }
+  }
 }
