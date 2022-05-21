c01b0a61094a1db0127b960f3700c61eb2777998
Evan <76844856+froggy1014@users.noreply.github.com>
2022-05-21 19:19:45 +0900

    modal redesion3

--- static/javascript/darkmode.js
+++ static/javascript/darkmode.js
@@ -58,13 +58,15 @@ function getDark() {
     $("#genre").css('border-bottom', '2px solid #30343f');
     $('#searchShop').css('color', '#30343f');
     $("#checkbox").css('background-color', "transparent");
-    $("button.submit").css('background-color', '#f7c72a');
+    $("button.submit").css('background-color', '#222c37');
     $(".modal-content").removeClass('modal-content-color');
+    $(".modal-content").addClass('modal-content-color2');
     $('body').css("background","#30343f")
+    $('.txt_field>input').css('background-color', '#dbdfe4')
   }
-  else if (page === "contact"){
+  else if (page === "feedback"){
     $('body').css("background","#4b652f");
-    $('.about-btn>button').css("background", "#6b9042");
+    $('.about-btn>button').css("background-color", "#6b9042");
   }
   else {
     if (page === "customer_request") {
@@ -110,22 +112,25 @@ function getBright() {
     $("#genre").css('border-bottom', '2px solid #f5f5f5');
     $('#searchShop').css('color', '#eb6383');
     $("button.submit").css('background-color', 'red');
+    $(".modal-content").removeClass('modal-content-color2');
     $(".modal-content").addClass('modal-content-color');
     $('body').css("background","white")
+    $('.txt_field>input').css('background-color', 'white')
+
   }
   else if (page === "customer_request") {
     $('.youtuberLink').css('color', 'black');
   }
-  else if (page === "contact"){
+  else if (page === "feedback"){
     $('body').css("background","#6a9141")
-    $('.about-btn>button').css("background", "#4b652e")
+    $('.about-btn>button').css("background-color", "#4b652e")
     
   }
   else {
     document.body.classList.remove("dark");
 
   }
-  $('#sidebar').css('background-color', 'white');
+  $('#sidebar').css('background-color', '#f7c72a');
   $("#upperSide").css('background-color', '#E2314B');
   $('.form-group').css('color', 'black');
   $("#checkbox").css('background-color', "transparent");
